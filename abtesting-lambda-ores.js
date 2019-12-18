fe28d8c6f4f73831492b43a9feaf1a0e58e7b628
gbts <gboutsioukis@gmail.com>
2019-12-18 19:02:08 +0200

    Create abtesting-lambda-ores.js

---
+++ abtesting-lambda-ores.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const COOKIE_KEY = 'abtesting-segmentid';
+
+const getCookie = (headers, cookieKey) => {
+    if (headers.cookie) {
+        for (let cookieHeader of headers.cookie) {
+            const cookies = cookieHeader.value.split(';');
+            for (let cookie of cookies) {
+                const [key, val] = cookie.split('=');
+                if (key === cookieKey) {
+                    return val;
+                }
+            }
+        }
+    }
+    return null;
+}
+
+// caveat: this will overwrite the set-cookie header from origin
+const setCookie = function(response, cookie) {
+    console.log(`Setting cookie ${cookie}`);
+    response.headers['set-cookie'] = [{ key: "Set-Cookie", value: cookie }];
+}
+
+exports.handler = (event, context, callback) => {
+    const request = event.Records[0].cf.request;
+    const headers = request.headers;
+    const response = event.Records[0].cf.response;
+
+    const cookieVal = getCookie(headers, COOKIE_KEY);
+    if (cookieVal != null) {
+        setCookie(response, `${COOKIE_KEY}=${cookieVal}`);
+        callback(null, response);
+        return;
+    }
+
+    console.log('no segmentid cookie');
+    callback(null, response);
+}
